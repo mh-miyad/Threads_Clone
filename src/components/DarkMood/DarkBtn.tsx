4fe079c81f944eac02e6ed951ce95b533309c43e
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2023-12-06 19:29:05 +0600

    fix dark button

--- src/components/DarkMood/DarkBtn.tsx
+++ src/components/DarkMood/DarkBtn.tsx
@@ -1,10 +1,10 @@
 "use client";
 import React, { useState } from "react";
-import { Switch } from "../ui/switch";
+import { HiOutlineSun } from "react-icons/hi2";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleTheme } from "@/Redux/DarkTheme/ThemeSlice";
 import { RootState } from "@/Redux/Store";
-
+import { BsMoonStars } from "react-icons/bs";
 const DarkBtn = () => {
   const isDark = useSelector((state: RootState) => state.theme.isDark);
   const dispatch = useDispatch();
@@ -14,7 +14,21 @@ const DarkBtn = () => {
 
   return (
     <div>
-      <Switch onClick={handleToggleTheme} />
+      {isDark ? (
+        <>
+          <button onClick={handleToggleTheme}>
+            <p className="sr-only">Dark Mood</p>
+            <BsMoonStars />
+          </button>
+        </>
+      ) : (
+        <>
+          <button onClick={handleToggleTheme}>
+            <p className="sr-only">Day Mood </p>
+            <HiOutlineSun />
+          </button>
+        </>
+      )}
     </div>
   );
 };
