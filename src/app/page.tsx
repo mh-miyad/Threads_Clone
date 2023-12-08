b4d7bd51f1157d265946cde30f7e620456d537d4
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2023-12-08 21:41:53 +0600

    post Tool Is Fix For large Device

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,11 @@ export default function Home() {
   return (
     <>
       <PostTool />
-      <div className="flex justify-center flex-col gap-10">
+      <div className="flex justify-center flex-col items-center">
+        <CardComp />
+        <CardComp />
+        <CardComp />
+        <CardComp />
         <CardComp />
       </div>
     </>
--- src/components/SticyPost/PostTool.tsx
+++ src/components/SticyPost/PostTool.tsx
@@ -40,9 +40,9 @@ const PostTool = () => {
           className="flex flex-col  flex-wrap "
         >
           {/* Register your input into the hook by invoking the "register" function */}
-          <div className="focus-within:ring focus-within:ring-fuchsia-400 dark:focus-within:ring-violet-600  border-2 border-purple-400 rounded-lg p-2 dark:border-violet-900 focus:outline-none scale-90 hover:scale-100 transition-all duration-200 ease-in-out">
+          <div className="focus-within:ring focus-within:ring-fuchsia-400 dark:focus-within:ring-violet-600  border-2 border-purple-400 rounded-lg p-2 dark:border-violet-900 focus:outline-none scale-90 hover:scale-100 transition-all duration-200 ease-in-out shadow-2xl shadow-violet-500/20">
             <textarea
-              className="resize-none w-full md:h-40 h-28 dark:bg-transparent border-none focus:outline-none py-7 px-10 scroll-smooth "
+              className="resize-none w-full  h-28 dark:bg-transparent border-none focus:outline-none py-7 px-10 scroll-smooth "
               placeholder="Write something..."
               {...register("post", { required: true })}
               maxLength={200}
@@ -80,7 +80,7 @@ const PostTool = () => {
               <div>
                 <input
                   type="submit"
-                  className=" bg-violet-600 hover:bg-violet-700 text-white font-bold rounded-full shadow-lg dark:shadow-purple-500/50 drop-shadow-2xl shadow-violet-600/50 w-28 h-10 uppercase scale-100 active:scale-90 transition-all duration-200 ease-in-out cursor-pointer mx-auto my-0 ml-8"
+                  className=" bg-violet-600 hover:bg-violet-700 text-white font-bold rounded-full shadow-lg dark:shadow-purple-500/50 drop-shadow-2xl shadow-violet-600/50 w-28 h-10 uppercase scale-100 active:scale-90 transition-all duration-200 ease-in-out cursor-pointer mx-auto mb-3"
                 />
               </div>
             </div>
