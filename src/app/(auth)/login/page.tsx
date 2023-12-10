8a76227bf8bafae3f059afe21653943791692864
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2023-12-10 19:53:11 +0600

    login and register page design is  complete

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,10 +2,20 @@
 import DarkBtn from "@/components/DarkMood/DarkBtn";
 import Link from "next/link";
 import React from "react";
-
+import { SubmitHandler, useForm } from "react-hook-form";
+type Inputs = {
+  email: string;
+  password: string;
+};
 const LoginPage = () => {
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
   };
   return (
     <div className=" relative">
@@ -13,40 +23,35 @@ const LoginPage = () => {
         <div></div>
         <DarkBtn />
       </div>
-      <div className="absolute top-0  translate-y-32 -translate-x-14 rotate-45 right-12 sm:right-40 bg-gradient-to-tl from-pink-500 to-indigo-600 w-96 h-10 blur-xl -z-30">
+      <div className="absolute top-24 right-0 bg-gradient-to-r from-pink-500 to-indigo-600 w-32 h-32 sm:w-52 sm:h-52 sm:right-20 sm:top-20 rounded-l-2xl blur-xl sm:blur-3xl -z-30 rounded-t-3xl -rotate-90 animate-pulse  delay-300 transition-all ">
         {" "}
       </div>
-      <div className="absolute top-0  translate-y-32 translate-x-14 -rotate-45 right-12 sm:right-40 bg-gradient-to-tl from-purple-700 to-fuchsia-600 w-96 h-10 blur-xl -z-30">
+      <div className="absolute bottom-24 left-0 bg-gradient-to-r from-indigo-500 to-rose-600 w-32 h-32 sm:w-52 sm:h-52 sm:left-20 sm:bottom-20  -z-30 rounded-t-3xl blur-xl rotate-90 rounded-r-2xl sm:blur-3xl animate-pulse  delay-150 transition-all ">
         {" "}
       </div>
-      <div className="absolute bottom-0  -translate-y-24 sm:right-40 -translate-x-14 -rotate-45 right-12 bg-gradient-to-tl from-purple-700 to-fuchsia-600 w-96 h-10 blur-xl -z-30">
-        {" "}
-      </div>
-      <div className="absolute bottom-0  -translate-y-24 sm:right-40 translate-x-14 rotate-45 right-12 bg-gradient-to-tl from-purple-700 to-fuchsia-600 w-96 h-10 blur-xl -z-30">
-        {" "}
-      </div>
-      <div className="flex justify-center h-[40vh] md:h-[60vh] items-center">
+
+      <div className="flex justify-center h-[80vh] md:h-[60vh] items-center">
         <div className="bg-black/10 dark:bg-white/5 backdrop-blur-3xl rounded-lg px-8 pt-10 pb-20  border-2 border-violet-500">
-          <h2 className="text-3xl bg-gradient-to-b  px-10 py-2 rounded-full text-white from-pink-500 to-violet-500 font-bold uppercase">
+          <h2 className="text-center md:text-3xl bg-gradient-to-b py-2 sm:px-10 sm:py-2 rounded-full text-white from-pink-500 to-violet-500 font-bold uppercase animate-pulse delay-500 transition-all">
             {" "}
             Please Login{" "}
           </h2>
 
           <div className="z-50 relative">
-            <form onSubmit={onSubmit}>
+            <form onSubmit={handleSubmit(onSubmit)}>
               <div className=" w-full max-w-xs mt-5">
                 <input
                   type="email"
                   placeholder="Email Address"
-                  formNoValidate
+                  {...register("email", { required: true })}
                   className=" w-full focus:outline-none h-10 py-6 border-2 border-violet-300 rounded-xl px-4 mb-4 scale-90 focus:scale-100 focus-within:border-violet-500 transition-all ease-linear duration-300 text-black "
                 />
               </div>
               <div className=" w-full max-w-xs mb-5">
                 <input
-                  type="email"
-                  placeholder="Email Address"
-                  formNoValidate
+                  type="password"
+                  placeholder="Password"
+                  {...register("password", { required: true })}
                   className=" w-full focus:outline-none h-10 py-6 border-2 border-violet-300 rounded-xl px-4  scale-90 focus:scale-100 focus-within:border-violet-500 transition-all ease-linear duration-300 text-black "
                 />
               </div>
@@ -54,14 +59,14 @@ const LoginPage = () => {
                 <div>{`Don't`} have an account?</div>
                 <Link
                   href={`/register`}
-                  className="underline underline-offset-8 text-violet-500 font-extrabold"
+                  className="underline underline-offset-8 text-violet-700 dark:text-violet-400/80 drop-shadow-2xl font-extrabold"
                 >
                   Register
                 </Link>
               </div>
               <button
                 type="submit"
-                className=" w-full max-w-xs border-2 border-fuchsia-500 dark:bg-white/10 backdrop-blur-3xl rounded-lg px-8 py-2  font-bold uppercase text-white active:scale-90 transition-all duration-200 ease-linear "
+                className=" w-full max-w-xs border-2 border-fuchsia-500 dark:bg-white/20  shadow-lg  backdrop-blur-3xl shadow-indigo-500/20  rounded-lg px-8 py-2  font-bold uppercase text-white active:scale-90 transition-all duration-200 ease-linear "
               >
                 Submit
               </button>
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,7 +1,90 @@
+"use client";
+import DarkBtn from "@/components/DarkMood/DarkBtn";
+import Link from "next/link";
 import React from "react";
-
+import { SubmitHandler, useForm } from "react-hook-form";
+type Inputs = {
+  name: string;
+  email: string;
+  password: string;
+};
 const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+  };
+  return (
+    <div className=" relative">
+      <div className="fixed z-50 top-10 right-12 flex justify-between mb-10">
+        <div></div>
+        <DarkBtn />
+      </div>
+      <div className="absolute top-24 right-0 bg-gradient-to-r from-violet-500 to-green-600 w-32 h-32 sm:w-52 sm:h-52 sm:right-20 sm:top-20 rounded-l-2xl blur-xl sm:blur-3xl -z-30 rounded-t-3xl -rotate-90 animate-pulse  delay-300 transition-all ">
+        {" "}
+      </div>
+      <div className="absolute bottom-24 left-0 bg-gradient-to-r from-indigo-500 to-emerald-400 w-32 h-32 sm:w-52 sm:h-52 sm:left-20 sm:bottom-20  -z-30 rounded-t-3xl blur-xl rotate-90 rounded-r-2xl sm:blur-3xl animate-pulse  delay-150 transition-all ">
+        {" "}
+      </div>
+
+      <div className="flex justify-center h-[80vh] md:h-[60vh] items-center">
+        <div className="bg-black/10 dark:bg-white/5 backdrop-blur-3xl rounded-lg px-8 pt-10 pb-20  border-2 border-green-500">
+          <h2 className="text-center md:text-3xl bg-gradient-to-r py-2 sm:px-10 sm:py-2 rounded-full text-white from-emerald-600  to-sky-500 font-bold uppercase animate-pulse">
+            {" "}
+            Register{" "}
+          </h2>
+
+          <div className="z-50 relative">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className=" w-full max-w-xs mt-5">
+                <input
+                  type="text"
+                  placeholder="Full Name"
+                  {...register("name", { required: true })}
+                  className=" w-full focus:outline-none h-10 py-6 border-2 border-violet-300 rounded-xl px-4 mb-4 scale-90 focus:scale-100 focus-within:border-indigo-500 transition-all ease-linear duration-300 text-black "
+                />
+              </div>
+              <div className=" w-full max-w-xs mt-5">
+                <input
+                  type="email"
+                  placeholder="Email Address"
+                  {...register("email", { required: true })}
+                  className=" w-full focus:outline-none h-10 py-6 border-2 border-violet-300 rounded-xl px-4 mb-4 scale-90 focus:scale-100 focus-within:border-indigo-500 transition-all ease-linear duration-300 text-black "
+                />
+              </div>
+              <div className=" w-full max-w-xs mb-5">
+                <input
+                  type="password"
+                  placeholder="Password"
+                  {...register("password", { required: true })}
+                  className=" w-full focus:outline-none h-10 py-6 border-2 border-violet-300 rounded-xl px-4  scale-90 focus:scale-100 focus-within:border-violet-500 transition-all ease-linear duration-300 text-black "
+                />
+              </div>
+              <div className=" w-full max-w-xs mb-5 px-4 flex gap-3 font-medium text-gray-800 dark:text-gray-200">
+                <div>Already have an account?</div>
+                <Link
+                  href={`/login`}
+                  className="underline underline-offset-8 text-emerald-700 dark:text-emerald-400 drop-shadow-2xl font-extrabold"
+                >
+                  Login
+                </Link>
+              </div>
+              <button
+                type="submit"
+                className=" w-full max-w-xs border-2 border-sky-500 dark:bg-white/20  shadow-lg  backdrop-blur-3xl shadow-sky-700/20  rounded-lg px-8 py-2  font-bold uppercase text-white active:scale-90 transition-all duration-200 ease-linear "
+              >
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RegisterPage;
