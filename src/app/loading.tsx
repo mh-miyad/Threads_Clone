0f99a52ecc7f56acc55be391bfd9d84cfd290390
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2023-12-13 20:40:51 +0600

    Loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const loading = () => {
-  return <div>loading.....................</div>;
+  return (
+    <div className="flex justify-center items-center h-screen text-5xl ">
+      loading.....................
+    </div>
+  );
 };
 
 export default loading;
