d9173307fb5825eb7c7e07e80a307ccd9be0d0f3
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2023-12-14 11:20:02 +0600

    add next configuration

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    optimizeCss: true,
+    proxyTimeout: 3000,
     serverActions: {
       allowedOrigins: [
         "https://coditalk.vercel.app/",
         "*https://coditalk.vercel.app/",
         "http://localhost:3000/",
       ],
+      bodySizeLimit: 1024 * 1024,
     },
   },
 };
