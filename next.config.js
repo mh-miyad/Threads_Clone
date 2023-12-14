c39ebdd6fb0d3245974c4c3139f68a7bd25fb053
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2023-12-14 11:14:13 +0600

    add here next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "https://coditalk.vercel.app/",
+        "*https://coditalk.vercel.app/",
+        "http://localhost:3000/",
+      ],
+    },
+  },
+};
 
 module.exports = nextConfig;
