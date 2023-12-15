19d504879db977b4ecea7fff4efa982ae4e64c48
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2023-12-15 23:35:54 +0600

    upload

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,19 +1,28 @@
-import cloudinary from "cloudinary";
-
+import { upload } from "./../../../Multer/multer";
+import { v2 as cloudinary } from "cloudinary";
 import { cloudinaryConfig } from "@/Cloudinary/Cloudinary";
 import { NextResponse } from "next/server";
 
 export const POST = async (req: Request, res: Response) => {
   const { file }: { file: File } = await req.json();
+  await cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    secure: true,
+  });
+  const options = {
+    use_filename: true,
+    unique_filename: false,
+    overwrite: true,
+  };
 
   try {
-    const uploadResponse = await cloudinary.v2.uploader.upload(
-      JSON.stringify(file),
-      cloudinaryConfig
-    );
-    console.log(uploadResponse);
-    NextResponse.json({ success: true, url: uploadResponse.secure_url });
-  } catch (error) {}
-  console.log(file);
-  NextResponse.json({ success: false });
+    // Upload the image
+    const result = await cloudinary.uploader.upload(file, options);
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
+  NextResponse.json({ success: true });
 };
