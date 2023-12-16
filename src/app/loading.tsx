38afaf93da9fd9b6559e844c782c29d8b8a0d42d
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2023-12-16 20:18:41 +0600

    add here loading Page

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,12 +1,18 @@
 import Image from "next/image";
 import React from "react";
 import logo1 from "../../public/C.svg";
-import logo2 from "../../public/CodiTalk.svg";
+
 const loading = () => {
   return (
-    <div className="flex justify-center items-center h-screen text-5xl ">
-      <Image alt="loading" src={logo1} />
-      <Image alt="loading" src={logo2} />
+    <div className="flex justify-center items-center h-[70vh] scale-50">
+      <Image
+        alt="loading"
+        src={logo1}
+        className="w-40 h-40 opacity-10 animate-pulse drop-shadow-2xl  "
+      />
+      <p className="text-5xl font-extrabold animate-pulse caption-top  text-clip text-transparent bg-clip-text bg-gradient-to-br from-indigo-500 to-purple-500 opacity-30 drop-shadow-2xl font-sans">
+        CodiTalk
+      </p>
     </div>
   );
 };
