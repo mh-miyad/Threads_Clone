dc3f03377a4b73117224a1c33e7065f5739ba7b6
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2023-12-24 20:03:00 +0600

    ui  add here

--- src/components/backdrop/BackdropFilter.tsx
+++ src/components/backdrop/BackdropFilter.tsx
@@ -3,10 +3,10 @@ import React from "react";
 const BackdropFilter = () => {
   return (
     <>
-      <div className="fixed top-80 right-0 bg-gradient-to-r from-violet-500 to-indigo-600 w-32 h-32 sm:w-80 sm:h-32 sm:right-20 sm:top-[300px] rounded-l-2xl blur-3xl  -z-30 rounded-t-3xl rotate-45 animate-pulse  delay-300 transition-all ">
+      <div className="fixed top-80 right-0 bg-gradient-to-r from-violet-600 to-indigo-500 w-32 h-32 sm:w-80 sm:h-32 sm:right-20 sm:top-[300px] rounded-l-2xl blur-3xl  -z-30 rounded-t-3xl rotate-45 animate-pulse  delay-300 transition-all ">
         {" "}
       </div>
-      <div className="fixed top-20 right-0 bg-gradient-to-r from-pink-600 to-orange-600 w-32 h-32 sm:w-80 sm:h-32 sm:right-20 sm:top-[600px] rounded-l-2xl blur-3xl  -z-30 rounded-t-3xl rotate-45 animate-pulse  delay-300 transition-all opacity-80 ">
+      <div className="fixed top-[420px] right-0 bg-gradient-to-r from-pink-600 to-orange-600 w-32 h-32 sm:w-80 sm:h-32 sm:right-20 sm:top-[600px] rounded-l-2xl blur-3xl  -z-30 rounded-t-3xl rotate-45 animate-pulse  delay-300 transition-all opacity-80 ">
         {" "}
       </div>
       <div className="fixed bottom-32 left-0 bg-gradient-to-r from-purple-500 to-rose-400 w-32 h-32 sm:w-96 sm:rounded-t-3xl  sm:h-20 sm:rotate-180 sm:left-20 sm:bottom-[160px]  -z-30 rounded-t-3xl  rotate-90 rounded-r-2xl blur-3xl animate-pulse  delay-150 transition-all ">
